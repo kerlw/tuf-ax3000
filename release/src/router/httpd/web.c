9350bf72d651afe348d1d491396ba38e675715ed
ZhaoWei Xu <paldier@hotmail.com>
2020-11-30 12:21:50 +0800

    Bumped revision to 5.1.2 final

--- release/src/router/httpd/web.c
+++ release/src/router/httpd/web.c
@@ -26884,7 +26884,6 @@ struct AiMesh_whitelist AiMesh_whitelists[] = {
 	{"message.htm", NULL},
 	{"error_page.htm", NULL},
 	{"Main_Login.asp", NULL},
-	{"Tools_Sysinfo.asp", NULL},
 	{ NULL, NULL }
 };
 #endif
